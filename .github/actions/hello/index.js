88c92c440e09e3b2ccb4e250812f3c9e0b7a69ff
Nagasree2000 <188w1a05b4@vrsiddhartha.ac.in>
2022-11-03 22:40:33 +0530

    new action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,13 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-try{
-const name = core.getInput('who-to-greet')
-console.log(`Hello ${name}`);
-const date = new Date();
-core.setOutput('Date',date.toTimeString());
+const core = require("@actions/core");
+const github = require("@actions/github");
+try {
+	const name = core.getInput("who-to-greet");
+	console.log(`Hello ${name}`);
+	const date = new Date();
+	core.setOutput("Date", date.toTimeString());
 
-console.log(JSON.stringify(github, null, '\t'));
-} catch(error)
-{
-    core.setFailed(error.message);
+	console.log(JSON.stringify(github, null, "\t"));
+} catch (error) {
+	core.setFailed(error.message);
 }
