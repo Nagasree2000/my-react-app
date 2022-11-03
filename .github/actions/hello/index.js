6aa8fb52d9ddac7450b442469530d22234b7cc2f
Nagasree2000 <188w1a05b4@vrsiddhartha.ac.in>
2022-11-03 22:58:13 +0530

    new action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,8 +3,8 @@ const github = require("@actions/github");
 try {
 	const name = core.getInput("who-to-greet");
 	console.log(`Hello ${name}`);
-	const date = new Date();
-	core.setOutput("Date", date.toTimeString());
+	const time = new Date();
+	core.setOutput("time", time.toTimeString());
 
 	console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
