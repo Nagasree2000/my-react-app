e7a77717923eddb6e80385b3051facaecaf0ae00
Nagasree2000 <188w1a05b4@vrsiddhartha.ac.in>
2022-11-04 15:44:35 +0530

    comitted

---
+++ .github/actions/slack/script.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once 'vendor/autoload.php';
+Requests::register_autoloader();
+
+var_dump( $argv );
+
+var_dump( $_ENV );
+
+/////$response = Requests::post(
+  //  "https://hooks.slack.com/services/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",
+    // array(
+      //  'content-type'=> 'application/json'
+     //),
+    //
+      //  json_encode(array(
+        //    'text'=> 'some message'
+        //),
+     //)
+       // );
+
+//var_dump($response);
+
