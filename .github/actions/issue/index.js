9c44cbdb1a8d3d64f0fbd42949fa98da2fd4db16
Nagasree2000 <188w1a05b4@vrsiddhartha.ac.in>
2022-11-04 09:54:51 +0530

    comitted

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,14 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { GitHub } = require("@actions/github/lib/utils");
+const { Octokit } = require("@octokit/rest");
 async function run() {
 	try {
 		const token = core.getInput("token");
 		const title = core.getInput("title");
 		const body = core.getInput("body");
 		const assignees = core.getInput("assignees");
-		const octokit = new GitHub(token);
-		const response = await octokit.rest.issues.create({
+		const octokit = new Octokit({
+			auth: token,
+		  });
+		  
+		const response = await octokit.issues.create({
 			owner: github.context.repo.owner,
 			repo: github.context.repo,
 			title,
